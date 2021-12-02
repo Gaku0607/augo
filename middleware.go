55000fd83989fe6d34d7917260c097ceb07975ac
Gaku0607 <71625869+Gaku0607@users.noreply.github.com>
2021-12-02 10:19:09 +0800

    fixed Recovery of function

--- middleware.go
+++ middleware.go
@@ -1,5 +1,7 @@
 package augo
 
+import "errors"
+
 //當所有Handler都執行完成時刪除Request所有檔案
 func DeletFiles() HandlerFunc {
 	return func(c *Context) {
@@ -14,9 +16,31 @@ func Recovery(log *Logger) HandlerFunc {
 	return func(c *Context) {
 		defer func() {
 			if r := recover(); r != nil {
-				log.Log(CreateLogParms(c.Request.Id, PANIC, c.Request.FilesName(), c.Request.method, LogKey{PANIC_ERROR: r.(error).Error()}))
-				c.AbortWithError(r.(error))
-				return
+
+				var (
+					key LogKey
+					err error
+				)
+
+				switch r.(type) {
+				case string:
+					err = errors.New(r.(string))
+					key = LogKey{PANIC_ERROR: err.Error()}
+
+				case error:
+					err = r.(error)
+					key = LogKey{PANIC_ERROR: err.Error()}
+
+				default:
+					err = errors.New("UNKNOW_ERROR")
+					key = LogKey{UNKNOW_ERROR: r}
+
+				}
+
+				deletFiles(c.Request.Files)
+
+				log.Log(CreateLogParms(c.Request.Id, PANIC, c.Request.FilesName(), c.Request.method, key))
+				c.AbortWithError(err)
 			}
 		}()
 		c.Next()
